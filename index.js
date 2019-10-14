5c480353dfd2e7ead15f40dc43d8a6f509b9b724
Pedro Araujo <pedro-foa@hotmail.com>
2019-10-14 19:59:20 -0300

    Created middlewares, put, post, delete

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const server = express();
 server.use(express.json());
 
 const projects = [];
+let countRequests = 0;
+
+server.use((req, res, next) => {
+  countRequests++;
+  console.log(`Número de requisições: ${countRequests}`);
+  return next();
+});
+
+function checkIdExistence(req, res, next) {
+  const { id } = req.params;
+  const project = projects.find(proj => proj.id == id);
+
+  if(!project) {
+    return res.status(400).json({ error: "This project doesnt exists" });
+  }
+
+  return next();
+}
 
 server.get('/projects', (req, res) => {
   return res.json(projects);
@@ -16,14 +34,42 @@ server.post('/projects', (req, res) => {
   projects.push({
     id,
     title,
-    tasks: []
+    tasks: [],
   });
 
   return res.json(projects);
 });
 
+server.put('/projects/:id', checkIdExistence, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
 
+  const project = projects.find(proj => proj.id = id);
 
+  project.title = title;
 
+  return res.json(project);
+});
+
+server.delete('/projects/:id', checkIdExistence, (req, res) => {
+  const { id } = req.params;
+
+  const project = projects.findIndex(proj => proj.id == id);
+
+  projects.splice(project, 1);
+
+  return res.send();
+});
+
+server.post('/projects/:id/tasks', checkIdExistence, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(proj => proj.id == id);
+
+  project.tasks.push(title);
+
+  return res.json(project);
+});
 
 server.listen(3000);
